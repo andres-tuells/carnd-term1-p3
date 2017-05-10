f97d94b3a4eec5f2a12ff818c358bd7369aeab78
Andres Tuells <zentropia@gmail.com>
2017-05-10 19:41:18 +0000

    trained

--- model.py
+++ model.py
@@ -182,7 +182,7 @@ def main():
     history_object = model.fit_generator(train_generator, 
         verbose=1, 
         validation_steps=len(validation_samples), 
-        epochs=2, 
+        epochs=1, 
         validation_data=validation_generator, 
         steps_per_epoch=len(train_samples)
     )
